2716b741c59e3226fbe509ef9b840188d76e3375
Luciano Ramalho <lramalho@thoughtworks.com>
2016-05-08 23:14:30 -0300

    adaptar numerar.py para gerar listagem para livreto impresso

--- numerar.py
+++ numerar.py
@@ -2,9 +2,15 @@
 
 """Numerar 7776 palavras de 11111 a 66666"""
 
+import sys
+import itertools
+
 PATH_PALAVRAS = '7776palavras.txt'
 
-import itertools
+if len(sys.argv) == 2 and sys.argv[1] == '3':
+    listar_3_dados = True
+else:
+    listar_3_dados = False
 
 with open(PATH_PALAVRAS, encoding='utf8') as fp:
     palavras = [p.strip() for p in fp]
@@ -12,4 +18,6 @@ with open(PATH_PALAVRAS, encoding='utf8') as fp:
 dados5 = list(''.join(dados) for dados in itertools.product('123456', repeat=5))
 
 for indice, palavra in zip(dados5, palavras):
+    if listar_3_dados:
+        indice = '[{}['.format(indice[-3:])
     print(indice, palavra)
